1de65cf5c2f348e4b13da73607ad62b09d2f14bf
zetta <vegaezequiel51@gmail.com>
2022-12-20 20:50:28 -0300

    Changed page home styles.module.css to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,20 @@ import styles from '../styles/pages/home.module.css'
 
 const Index = async () => {
   return <>
-    <Banner backgroundImage="/img/placeholders/bannerDesktop.png" title="Bienvenidos a UnBit" subtitle="Un bit a la vez" />
-     {/* 
-     <div className={styles.container}>
-        <div className={styles.containerContent}>
-          <section>
-            <p className={styles.introduction}>UnBit es un blog donde podrás encontrar varios
+    <Banner backgroundImage="/img/placeholders/bannerDesktop.png" title="Bienvenidos a UnBit" subtitle="Un bit a la vez" /> 
+     <div className="grid justify-items-center w-full">
+        <div className="w-2/3 max-md:w-4/5">
+          <section className="py-10 max-md:py-5">
+            <p className="text-4xl max-md:text-xl">UnBit es un blog donde podrás encontrar varios
               Posts de programación de frameworks favoritos y curiosidades
               sobre ellos, así como varios tutoriales explicados desde cero
               para que te conviertas en un verdadero experto.</p>
           </section>
-          <section className={styles.aboutUs}>
-            <h4 className={styles.titleAboutUs}>Quien Soy?</h4> 
-            <div className={styles.containerAboutUs}>
+          <section className="grid justify-items-center mb-8">
+            <h4 className="text-5xl mb-10 max-md:text-4xl">Quien Soy?</h4> 
+            <div className="grid grid-cols-2 gap-8 max-md:grid-cols-1">
               <Image src="/img/placeholders/perfil.jpeg" alt="" width={500} height={500} className={styles.image} />
-              <p className={styles.description}>
+              <p className="text-2xl">
                 Hola! Soy <strong>Ezequiel</strong> me dicen "Zetta" un desarollador Frontend y estudiante de Desarollo de Video juegos.
                 Comenze este blog para compartir mis conocimientos y dia al dia como desarrollador con ustedes,
                 trabajo como Frontend Developer hace mas de 4 años y a lo largo de mi corta carrera como dev me
@@ -30,15 +29,12 @@ const Index = async () => {
                 y creando este blog.
                 <br />
                 <br />
-                <br />
-                <br />
                 Saludo su amigo <strong>Zetta!</strong>
               </p>
             </div>
           </section>
         </div>
-     </div>
-     */}
+     </div> 
   </>
 }
 
